2229ec9556dabf732d704f1c8e98f53f806149f9
Trung Kiên <92300304+kientthe163256@users.noreply.github.com>
2022-09-20 13:36:37 +0700

    Create new project

---
+++ src/main/java/com/example/studentmanagementfa22/StudentManagementFa22Application.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementfa22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementFa22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementFa22Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentmanagementfa22/StudentManagementFa22ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmanagementfa22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementFa22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
