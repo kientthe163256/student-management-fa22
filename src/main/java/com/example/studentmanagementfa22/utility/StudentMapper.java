adffaaa2e7ecff2c6d26f76d9194d1c2678b5f96
TU <tumoonhanh@gmail.com>
2022-11-02 14:47:39 +0700

    change studentMapper

--- src/main/java/com/example/studentmanagementfa22/utility/StudentMapper.java
+++ src/main/java/com/example/studentmanagementfa22/utility/StudentMapper.java
@@ -1,9 +1,9 @@
 package com.example.studentmanagementfa22.utility;
+
 import com.example.studentmanagementfa22.dto.StudentDTO;
-import com.example.studentmanagementfa22.entity.Account;
+import com.example.studentmanagementfa22.entity.Student;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
-public interface StudentMapper extends IGenericMapper<Account, StudentDTO>{
-
+public interface StudentMapper extends IGenericMapper<Student, StudentDTO>{
 }
